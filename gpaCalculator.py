720d7ed285f3e3c4bd750cbb137ec4c8a38952ec
정현규 <jeonghyeongyu@jeonghycBookPro.JOOWIN>
2023-10-01 10:43:16 -0700

    color changed

--- gpaCalculator.py
+++ gpaCalculator.py
@@ -10,19 +10,19 @@ class MyGUI:
         self.mainWindow.geometry(f"380x{self.windowHeight}+720+50")
         self.mainWindow.title("GPA Calculator")
         self.gradeValue = ["", "A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "F"]
-        self.frame1 = Frame(self.mainWindow, width=246, height=30, bg="#e47777")
+        self.frame1 = Frame(self.mainWindow, width=246, height=30, bg="#d2302c")
         self.frame1.place(x=20, y=30)
         self.frame2Height = 100 # variable to resize frame2 height
-        self.frame2 = Frame(self.mainWindow, width=246, height=self.frame2Height, bg="#f9b5b5")
+        self.frame2 = Frame(self.mainWindow, width=246, height=self.frame2Height, bg="#f4a896")
         self.frame2.place(x=20, y=60)
         self.frame3 = Frame(self.mainWindow, width=100, height=180)
         self.frame3.place(x=266, y=30)
     
-        self.courseLabel = Label(self.frame1, text="Course", font=14, fg="black", bg="#e47777")
+        self.courseLabel = Label(self.frame1, text="Course", font=14, fg="white", bg="#d2302c")
         self.courseLabel.place(x=10, y=5)
-        self.gradeLabel = Label(self.frame1, text="Grade", font=14, fg="black", bg="#e47777")
+        self.gradeLabel = Label(self.frame1, text="Grade", font=14, fg="white", bg="#d2302c")
         self.gradeLabel.place(x=113, y=5)
-        self.includeLabel = Label(self.frame1, text="Included", font=14, fg="black", bg="#e47777")
+        self.includeLabel = Label(self.frame1, text="Included", font=14, fg="white", bg="#d2302c")
         self.includeLabel.place(x=180, y=5)
         
         
@@ -38,14 +38,14 @@ class MyGUI:
 
         
         
-        self.course = Entry(self.frame2, width=10, bg="#f9b5b5", fg="black")
+        self.course = Entry(self.frame2, width=10, bg="#f4a896", fg="black")
         self.course.place(x=10, y=5)
         self.grade = ttk.Combobox(self.frame2, values=self.gradeValue, width=3)
         self.grade.place(x=113, y=5)
         self.grade.set(self.gradeValue[0])
         self.include=IntVar()
         self.include.set(1)
-        self.cb = Checkbutton(self.frame2, width=3, variable=self.include, font=14, bg="#f9b5b5")
+        self.cb = Checkbutton(self.frame2, width=3, variable=self.include, font=14, bg="#f4a896")
         self.cb.place(x=195, y=5)
         
 
@@ -65,52 +65,52 @@ class MyGUI:
         self.checkBg = 0 # check background color
         
     def changeBgr(self): # change background red
-        self.frame1.config(bg="#e47777")
-        self.frame2.config(bg="#f9b5b5")
-        self.course.config(bg="#f9b5b5")
-        self.cb.config(bg="#f9b5b5")
-        self.courseLabel.config(bg="#e47777")
-        self.gradeLabel.config(bg="#e47777")
-        self.includeLabel.config(bg="#e47777")
+        self.frame1.config(bg="#d2302c")
+        self.frame2.config(bg="#f4a896")
+        self.course.config(bg="#f4a896")
+        self.cb.config(bg="#f4a896")
+        self.courseLabel.config(bg="#d2302c")
+        self.gradeLabel.config(bg="#d2302c")
+        self.includeLabel.config(bg="#d2302c")
         
         
         for i in self.courseList:
-            i.config(bg="#f9b5b5")
+            i.config(bg="#f4a896")
         for i in self.cbList:
-            i.config(bg="#f9b5b5")
+            i.config(bg="#f4a896")
         self.checkBg = 0
         
         
     def changeBgg(self): # change background green
-        self.frame1.config(bg="#8abd5f")
-        self.frame2.config(bg="#bae397")
-        self.course.config(bg="#bae397")
-        self.cb.config(bg="#bae397")
-        self.courseLabel.config(bg="#8abd5f")
-        self.gradeLabel.config(bg="#8abd5f")
-        self.includeLabel.config(bg="#8abd5f")
+        self.frame1.config(bg="#17553b")
+        self.frame2.config(bg="#ced46a")
+        self.course.config(bg="#ced46a")
+        self.cb.config(bg="#ced46a")
+        self.courseLabel.config(bg="#17553b")
+        self.gradeLabel.config(bg="#17553b")
+        self.includeLabel.config(bg="#17553b")
         
 
         for i in self.courseList:
-            i.config(bg="#bae397")
+            i.config(bg="#ced46a")
         for i in self.cbList:
-            i.config(bg="#bae397")
+            i.config(bg="#ced46a")
         self.checkBg = 1
     
     def changeBgb(self): # change background blue
-        self.frame1.config(bg="#7abdc2")
-        self.frame2.config(bg="#bbf0f2")
-        self.course.config(bg="#bbf0f2")
-        self.cb.config(bg="#bbf0f2")
-        self.courseLabel.config(bg="#7abdc2")
-        self.gradeLabel.config(bg="#7abdc2")
-        self.includeLabel.config(bg="#7abdc2")
+        self.frame1.config(bg="#1663b2")
+        self.frame2.config(bg="#9cc3d5")
+        self.course.config(bg="#9cc3d5")
+        self.cb.config(bg="#9cc3d5")
+        self.courseLabel.config(bg="#1663b2")
+        self.gradeLabel.config(bg="#1663b2")
+        self.includeLabel.config(bg="#1663b2")
         
 
         for i in self.courseList:
-            i.config(bg="#bbf0f2")
+            i.config(bg="#9cc3d5")
         for i in self.cbList:
-            i.config(bg="#bbf0f2")
+            i.config(bg="#9cc3d5")
         self.checkBg = 2
     
     def addCourse(self):
@@ -140,21 +140,21 @@ class MyGUI:
         
         if self.checkBg == 0:
             for i in self.courseList:
-                i.config(bg="#f9b5b5")
+                i.config(bg="#f4a896")
             for i in self.cbList:
-                i.config(bg="#f9b5b5")
+                i.config(bg="#f4a896")
 
         elif self.checkBg == 1:
             for i in self.courseList:
-                i.config(bg="#bae397")
+                i.config(bg="#ced46a")
             for i in self.cbList:
-                i.config(bg="#bae397")
+                i.config(bg="#ced46a")
          
         elif self.checkBg == 2:
             for i in self.courseList:
-                i.config(bg="#bbf0f2")
+                i.config(bg="#9cc3d5")
             for i in self.cbList:
-                i.config(bg="#bbf0f2")
+                i.config(bg="#9cc3d5")
 
             
 
